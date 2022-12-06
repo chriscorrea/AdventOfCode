850e17f7134646394357e76e5bd54b6a9652fdcf
Chris Correa <ccorrea@codeforamerica.org>
2022-12-05 21:48:18 -0800

    2022-12-06: Enigma Part 2

--- 2022/6/enigma.py
+++ 2022/6/enigma.py
@@ -3,3 +3,4 @@
  
 data = open('../inputs/day6.txt').read()
 print(f"Part 1 Solution: {[len(set(data[i:i+4]))for i, val in enumerate(data)].index(4)+4}")
+print(f"Part 2 Solution: {[len(set(data[i:i+14]))for i, val in enumerate(data)].index(14)+14}")
