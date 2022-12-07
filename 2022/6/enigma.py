65d1bc0f76562a2166d5971be8e32ce851f772b8
Chris Correa <ccorrea@codeforamerica.org>
2022-12-06 19:10:16 -0800

    save 8 bytes of disk space

--- 2022/6/enigma.py
+++ 2022/6/enigma.py
@@ -2,5 +2,5 @@
 # https://adventofcode.com/2022/day/6
  
 data = open('../inputs/day6.txt').read()
-print(f"Part 1 Solution: {[len(set(data[i:i+4]))for i, val in enumerate(data)].index(4)+4}")
-print(f"Part 2 Solution: {[len(set(data[i:i+14]))for i, val in enumerate(data)].index(14)+14}")
+print(f"Part 1 Solution: {[len(set(data[i:i+4]))for i in range(len(data))].index(4)+4}")
+print(f"Part 2 Solution: {[len(set(data[i:i+14]))for i in range(len(data))].index(14)+14}")
